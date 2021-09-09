3d448559ecd97517b99b3768bc518fdeb84e9841
tumusx <tumusx1@gmail.com>
2021-09-09 17:57:46 -0300

    Created class and operator

---
+++ src/aprendizado/pacote/TesteNomes.kt
@@ -0,0 +1,28 @@
+package aprendizado.pacote
+
+import java.util.*
+
+fun nomeRandon() : String{
+   val semaRandon= arrayOf("Joao", "Carlos", "Silvio", "Raimundo", "Joaquin")
+    return semaRandon[Random().nextInt(semaRandon.size)]
+}
+
+fun sigNome(names: String) : String{
+   return when(names){
+       "Joao" -> "Sagitario"
+       "Carlos"-> "Touro"
+       "Silvio"-> "Libras"
+       "Raimundo"-> "Gemeos"
+       "Joaquin"-> "Escorpiao"
+       else -> "Nada"
+   }
+}
+fun aglobarVals(){
+    val nomeSigno = nomeRandon()
+    val food = sigNome(nomeSigno)
+    println("Quem se chama $nomeSigno pertence ao signo $food ")
+
+}
+fun main(args: Array<String>){
+    aglobarVals()
+}
