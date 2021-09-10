ea9d7536d912047cb9ce112a14ebc2cd015b5d2e
tumusx <tumusx1@gmail.com>
2021-09-09 22:48:59 -0300

    update

--- src/aprendizado/pacote/TesteNomes.kt
+++ src/aprendizado/pacote/TesteNomes.kt
@@ -1,4 +1,5 @@
 package aprendizado.pacote
+import java.util.Scanner
 
 import java.util.*
 
@@ -17,12 +18,46 @@ fun sigNome(names: String) : String{
        else -> "Nada"
    }
 }
+/*fun aglobarVals(){
+    val nomeSigno = nomeRandon()
+    val food = sigNome(nomeSigno)
+    val idade = 15
+    println("idade é verdadeira....${aprovacaoPar(idade)}")
+    println("Quem se chama $nomeSigno pertence ao signo $food ")
+
+}*/
+    /*fun main(args: Array<String>){
+    aglobarVals()*/
+
+
+fun exp(rapido: String = "fast") { //criando uma variavel padrão para o parametro --> vamos evitar de escrever muitas ver~soes de sobrecarga do mesmo código//
+    println("rapido $rapido")
+
+
+}
+fun aprovacaoPar(idade: Int=15, seriEscolar:Int= 2, termiCo: String= "30"): Boolean{
+    return when{
+        idade >= 15 -> false
+        seriEscolar > 2 -> false
+        termiCo >="30" -> true
+
+      else-> true
+    }
+
+
+}
 fun aglobarVals(){
     val nomeSigno = nomeRandon()
     val food = sigNome(nomeSigno)
+    val idade = 15
+    println("idade é verdadeira....${aprovacaoPar(idade)}")
     println("Quem se chama $nomeSigno pertence ao signo $food ")
 
 }
 fun main(args: Array<String>){
+    aprovacaoPar()
     aglobarVals()
+    exp() //chamando a função sem nada
+    exp("devagar") //chamando a função e passando sem o nomear o parametro
+    exp(rapido= "como tartaruga") //chamando a função e nomeando o parametro --> rapido
 }
